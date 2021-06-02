4a2960b302ba3bb1d7ea22140813b7be4c835aac
Dmitrii Konevnin <lilithillusion@gmail.com>
2021-06-02 15:25:21 +0300

    Уберет ошибки линтера

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ const getRandomNumber = function(minNumber,maxNumber) {
   }
 
   return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-}
+};
 const getRandomCoordinates  = function  (minCoordinate, maxCoordinate, numberAfterDots)  {
   if (minCoordinate < 0 || maxCoordinate < 0 || maxCoordinate <= minCoordinate) {
     return;
   }
   const originalNumber = (Math.random() * (maxCoordinate - minCoordinate + 1)) + minCoordinate;
   return originalNumber.toFixed(numberAfterDots);
-}
+};
 getRandomNumber();
 getRandomCoordinates();
 
