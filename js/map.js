f1a153cb2aa2df5e11e73229fc0edf46750a069f
Dmitrii Konevnin <lilithillusion@gmail.com>
2021-07-04 23:33:23 +0300

    Исправляет ошибу в адресе иконок меток

--- js/map.js
+++ js/map.js
@@ -18,7 +18,7 @@ L.tileLayer(
   },
 ).addTo(mapTokio);
 const customPinIcons = L.icon({
-  iconUrl: '/img/main-pin.svg',
+  iconUrl: 'img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -42,7 +42,7 @@ const amountCards = 10;
 createArrayCards(amountCards).forEach((point) => {
   const {lat, lng} = point.location;
   const icon = L.icon({
-    iconUrl: '/img/pin.svg',
+    iconUrl: 'img/pin.svg',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
   });
